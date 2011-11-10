7e60dea167e168c5084185b37bc1fd68a0e65909
Randy McMillan <randy.lee.mcmillan@gmail.com>
2011-11-10 17:43:54 -0500

    Init Commit of functioning ChildBrowser Example
    Init Commit of functioning ChildBrowser Example

---
+++ ChildBrowserTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ChildBrowserTest
+//
+//  Created by Randy McMillan on 11/10/11.
+//  Copyright OpenOSX.org 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
